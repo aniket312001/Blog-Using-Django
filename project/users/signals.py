1b6018a6e2daa4ee05e1e5b99853cec2ede729c7
aniket <aniketchavan505@gmail.com>
2020-12-20 13:28:39 +0530

    Commit

---
+++ project/users/signals.py
@@ -0,0 +1,19 @@
+# When ever we register new user it will give also create it will also create a profile row in db
+# by this not need to go manually and add create new row entry in Profile table and add user 
+
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
